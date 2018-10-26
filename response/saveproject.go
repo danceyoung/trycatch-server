8f4efc14f42afc42ed64028df5ad9ec35885b550
Young <danceyoung@hotmail.com>
2018-10-26 16:16:20 +0800

    update

--- response/saveproject.go
+++ response/saveproject.go
@@ -9,14 +9,17 @@
 package response
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/danceyoung/trycatchserver/db"
 	"github.com/danceyoung/trycatchserver/model"
+	"github.com/gin-gonic/gin"
 )
 
 func SaveProject(project model.EditProject) map[string]interface{} {
 	//save project ,delete members,update members,insert members
+	fmt.Println("invoke saving when editing a project")
 	var updateProjectSql = "update tt_project set project_name=?,language=? where id<>0 and project_id=? and created_by=?"
 	stmt, err := db.DB.Prepare(updateProjectSql)
 	if err == nil {
@@ -43,20 +46,14 @@ func SaveProject(project model.EditProject) map[string]interface{} {
 		}
 	}
 
-	var genmembers []model.EditMember
-
 	for i := 0; i < len(project.Members); i++ {
 		member := project.Members[i]
-		if len(member.UserId) > 0 {
-			var updateMemberSql = "update tt_project_member set receive_from_list='" + member.ReceiveFromList + "' where project_id='" + project.ProjectId + "' and user_id='" + member.UserId + "'"
-			_, err := db.DB.Exec(updateMemberSql)
-			if err != nil {
-				panic(err.Error())
-			}
-		} else {
-			genmembers = append(genmembers, member)
+		var updateMemberSql = "update tt_project_member set receive_from_list='" + member.ReceiveFromList + "' where project_id='" + project.ProjectId + "' and user_id='" + member.UserId + "'"
+		_, err := db.DB.Exec(updateMemberSql)
+		if err != nil {
+			panic(err.Error())
 		}
 	}
 
-	return InsertMemberAction(genmembers, project.ProjectId)
+	return gin.H{"msg": gin.H{"code": 0, "content": "edit&save project succeessfully."}}
 }
