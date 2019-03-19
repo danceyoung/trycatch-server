6b2357a95a1ccdd78f8e71735075bbe4f52763c9
Young <danceyoung@hotmail.com>
2019-03-19 16:47:46 +0800

    add user method

--- model/user.go
+++ model/user.go
@@ -8,6 +8,13 @@
  */
 package model
 
+import (
+	"database/sql"
+	"encoding/base64"
+
+	"github.com/danceyoung/trycatchserver/db"
+)
+
 type SigninUser struct {
 	AccountName string `json:"account_name" binding:"required"`
 	Password    string `json:"password" binding:"required"`
@@ -21,4 +28,32 @@ type ChangePassword struct {
 	UserId string `json:"uid" binding:"required"`
 	Old    string `json:"old" binding:"required"`
 	New    string `json:"new" binding:"required"`
+}
+
+type User struct {
+	UserName, Password string
+}
+
+func (user User) IsExisted() (bool, string, string) {
+	var (
+		userid       string
+		scanpassword string
+	)
+	err := db.DB.QueryRow("select user_id, password from tt_user where account_name = ?", user.UserName).Scan(&userid, &scanpassword)
+	if err == sql.ErrNoRows {
+		return false, "", ""
+	} else if err != nil {
+		panic(err)
+	} else {
+		return true, userid, scanpassword
+	}
+}
+
+func (user User) AddUser() string {
+	base64uid := base64.RawURLEncoding.EncodeToString([]byte(user.UserName))
+	_, err := db.DB.Exec("insert into tt_user (`user_id`, `account_name`, `password`) values (?,?,?)", base64uid, user.UserName, user.Password)
+	if err == nil {
+		return base64uid
+	}
+	return ""
 }
