cad26b142e383216f1fe5aa0baa20f102fa63c6e
Young <danceyoung@hotmail.com>
2019-03-25 16:29:02 +0800

    update xgpush tokenlist

--- response/trycatch.go
+++ response/trycatch.go
@@ -4,11 +4,12 @@
  * @flow
  * @Date: 2018-08-08 09:38:15
  * @Last Modified by: Young
- * @Last Modified time: 2018-08-27 16:08:29
+ * @Last Modified time: 2019-03-25 15:51:39
  */
 package response
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/danceyoung/trycatchserver/constant"
@@ -36,7 +37,44 @@ func TryCatch(header model.Header, info string) map[string]interface{} {
 	stmt, err := db.DB.Prepare("insert into tt_catch_info (`user_id`,`project_id`,`catch_info`) values (?,?,?)")
 	defer stmt.Close()
 	if err == nil {
-		stmt.Exec(userid, projectid, info)
+		_, errIns := stmt.Exec(userid, projectid, info)
+		if errIns == nil {
+			var uidsArray []string
+			var currentAccountName string
+			errCAN := db.DB.QueryRow("select account_name from tt_user where user_id = ?", userid).Scan(&currentAccountName)
+			if errCAN != nil {
+				panic(errCAN.Error())
+			} else {
+				sql := "select user_id from tt_project_member where receive_from_list like '%?%'"
+				sql = strings.Replace(sql, "?", currentAccountName, -1)
+				fmt.Println(sql)
+				rows, errUids := db.DB.Query(sql)
+				if errUids != nil {
+					panic(errUids.Error())
+				}
+				defer rows.Close()
+				for rows.Next() {
+					var tempUid string
+					if errTempUid := rows.Scan(&tempUid); errTempUid != nil {
+						panic(errTempUid.Error())
+					}
+					uidsArray = append(uidsArray, tempUid)
+				}
+			}
+			var debuger = model.ProjectMemberAlias(userid)
+			var projectName = model.ProjectName(projectid)
+			var title = debuger + " " + projectName
+			var deviceTokens []string
+			for i := 0; i < len(uidsArray); i++ {
+				var userid = uidsArray[i]
+				var deviceToken = model.DeviceTokenByUserId(userid)
+				deviceTokens = append(deviceTokens, deviceToken)
+			}
+			XgPush(deviceTokens, title, info)
+
+		} else {
+			panic(errIns.Error())
+		}
 	} else {
 		panic(err.Error())
 	}
