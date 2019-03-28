e28ca5852b6cfe0dc807340c236de07e0dcbf93f
Young <danceyoung@hotmail.com>
2019-03-28 16:41:13 +0800

    add update lastedbugstate fetched

--- response/projectbugs.go
+++ response/projectbugs.go
@@ -4,7 +4,7 @@
  * @flow
  * @Date: 2018-08-23 14:41:44
  * @Last Modified by: Young
- * @Last Modified time: 2018-08-23 17:28:49
+ * @Last Modified time: 2019-03-28 16:38:31
  */
 package response
 
@@ -30,18 +30,25 @@ func Bugs(req model.ProjectBugRequest) map[string]interface{} {
 		panic(err.Error())
 	} else {
 		userids := "'" + strings.Join(req.DebuggerIds, "','") + "'"
-		rows, err := db.DB.Query("select tt_catch_info.user_id,tt_project_member.user_alias,catch_info from tt_catch_info,tt_project_member where tt_catch_info.user_id =tt_project_member.user_id and  tt_catch_info.project_id =tt_project_member.project_id and tt_catch_info.user_id in (" + userids + ") and tt_catch_info.project_id ='" + req.ProjectId + "' limit " + strconv.Itoa((req.FetchPage-1)*constant.ItemsCountPerPage) + ", " + strconv.Itoa(constant.ItemsCountPerPage))
-		fmt.Println("select tt_catch_info.user_id,tt_project_member.user_alias,catch_info from tt_catch_info,tt_project_member where tt_catch_info.user_id =tt_project_member.user_id and  tt_catch_info.project_id =tt_project_member.project_id and tt_catch_info.user_id in (" + userids + ") and tt_catch_info.project_id ='" + req.ProjectId + "' limit " + strconv.Itoa((req.FetchPage-1)*constant.ItemsCountPerPage) + ", " + strconv.Itoa(constant.ItemsCountPerPage))
+		//客户端不断加载更多的时候，会造成重复，如果此时已经新增许多记录到表中
+		rows, err := db.DB.Query("select tt_catch_info.id, tt_catch_info.user_id,tt_project_member.user_alias,catch_info from tt_catch_info,tt_project_member where tt_catch_info.user_id =tt_project_member.user_id and  tt_catch_info.project_id =tt_project_member.project_id and tt_catch_info.user_id in (" + userids + ") and tt_catch_info.project_id ='" + req.ProjectId + "' order by tt_catch_info.id desc limit " + strconv.Itoa((req.FetchPage-1)*constant.ItemsCountPerPage) + ", " + strconv.Itoa(constant.ItemsCountPerPage))
+		fmt.Println("select tt_catch_info.id, tt_catch_info.user_id,tt_project_member.user_alias,catch_info from tt_catch_info,tt_project_member where tt_catch_info.user_id =tt_project_member.user_id and  tt_catch_info.project_id =tt_project_member.project_id and tt_catch_info.user_id in (" + userids + ") and tt_catch_info.project_id ='" + req.ProjectId + "' order by tt_catch_info.id desc limit " + strconv.Itoa((req.FetchPage-1)*constant.ItemsCountPerPage) + ", " + strconv.Itoa(constant.ItemsCountPerPage))
 		defer rows.Close()
 		if err != nil {
 			panic(err.Error())
 		} else {
 			var bugsAry []map[string]string
+			var i = 0
 			for rows.Next() {
 				var userId, alias, content string
-				if err := rows.Scan(&userId, &alias, &content); err != nil {
+				var id int
+				if err := rows.Scan(&id, &userId, &alias, &content); err != nil {
 					panic(err.Error())
 				} else {
+					if i == 0 {
+						UpdateLastedBugDateFetched(req.UserId, req.ProjectId, id)
+					}
+					i++
 					var bug = make(map[string]string)
 					bug["user_id"] = userId
 					bug["alias"] = alias
