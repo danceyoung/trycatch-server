7760e07c103c0fe899580b3a9b6e4913d376a545
Young <danceyoung@hotmail.com>
2019-04-11 10:31:33 +0800

    modify json property

--- model/header.go
+++ model/header.go
@@ -4,10 +4,11 @@
  * @flow
  * @Date: 2018-08-07 17:15:23
  * @Last Modified by: Young
- * @Last Modified time: 2018-08-27 15:57:04
+ * @Last Modified time: 2019-04-02 14:51:57
  */
 package model
 
 type Header struct {
-	Ttftoken string `json:"ttf_token"`
+	TtfAccessToken  string `json:"ttf_access_token"`
+	TtfLogTimestamp int    `json:"ttf_log_timestamp"`
 }
